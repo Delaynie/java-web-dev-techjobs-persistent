0dad6685bb0bdd04137f16ed43fd3b4cdfcfbc63
Delaynie <apple@Delaynies-MBP.attlocal.net>
2021-03-30 10:15:40 -0500

    Part 4 sql task finishedgit add .

--- queries.sql
+++ queries.sql
@@ -14,7 +14,8 @@ DROP TABLE job;
 
 ## Part 4: Test it with SQL
 
-SELECT name, description
+SELECT DISTINCT name, description
 FROM skill
-WHERE id IS NOT NULL
-ORDER BY name asc;
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
